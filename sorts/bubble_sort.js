71edc38af29ab8f71186837984a37aa8a4da205a
AdhishN <adhishn007@gmail.com>
2023-03-12 14:05:02 +0530

    added bootstrap and colors

--- sorts/bubble_sort.js
+++ sorts/bubble_sort.js
@@ -1,25 +1,39 @@
-// bubble-sort.js
-
 const bubbleSort = async (arrayElements) => {
-    const n = arrayElements.length;
-  
-    // Bubble sort algorithm
-    for (let i = 0; i < n - 1 && isSorting; i++) {
-      for (let j = 0; j < n - i - 1 && isSorting; j++) {
-        const currentElement = arrayElements[j];
-        const nextElement = arrayElements[j + 1];
-        currentElement.classList.add('active');
-        nextElement.classList.add('active');
-        await new Promise((resolve) => setTimeout(resolve, 50));
-        const currentValue = parseInt(currentElement.style.height);
-        const nextValue = parseInt(nextElement.style.height);
-        if (currentValue > nextValue) {
-          currentElement.style.height = `${nextValue}px`;
-          nextElement.style.height = `${currentValue}px`;
-        }
-        currentElement.classList.remove('active');
-        nextElement.classList.remove('active');
+  const n = arrayElements.length;
+
+  // Outer loop to iterate through all elements
+  for (let i = 0; i < n - 1 && isSorting; i++) {
+    // Inner loop to compare adjacent elements
+    for (let j = 0; j < n - i - 1 && isSorting; j++) {
+      // Highlight the two elements being compared
+      const currentElement = arrayElements[j];
+      const nextElement = arrayElements[j + 1];
+      currentElement.classList.add('active');
+      nextElement.classList.add('active');
+
+      // Wait for a short period to show the comparison
+      await new Promise((resolve) => setTimeout(resolve, 1));
+
+      // Get the heights of the two elements being compared
+      const currentValue = parseInt(currentElement.style.height);
+      const nextValue = parseInt(nextElement.style.height);
+
+      // Swap the elements if they are in the wrong order
+      if (currentValue > nextValue) {
+        currentElement.style.height = `${nextValue}px`;
+        nextElement.style.height = `${currentValue}px`;
       }
+
+      // Remove the highlighting from the two elements
+      currentElement.classList.remove('active');
+      nextElement.classList.remove('active');
     }
-  };
-  
+    arrayElements[n - i - 1].classList.add('sorted'); // Add sorted class to the last element sorted in the iteration
+  }
+  if (isSorting){
+    arrayElements[0].classList.add('sorted'); // Add sorted class to the first element in the sorted array
+  }
+};
+
+/* Note: isSorting is a global variable that is set to false when the sorting stops.
+This is done so that the sorting can be stopped by the user when needed. */
