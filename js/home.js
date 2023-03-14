1cfd730718531dadb07e369c8d5edad3e9368994
AdhishN <adhishn007@gmail.com>
2023-03-14 16:12:19 +0530

    aesthetic changes

--- js/home.js
+++ js/home.js
@@ -12,7 +12,7 @@ const generateArray = () => {
     // Get the array size from the input field
     const arraySize = Number(document.getElementById('array-size-btn').value);
 	// Generate an array of random numbers between 1 and 100
-	const array = Array.from({ length: arraySize}, () => Math.floor(Math.random() * 100) + 1);
+	const array = Array.from({ length: arraySize}, () => Math.floor(Math.random() * 150) + 1);
 	// Clear the previous array
 	arrayContainer.innerHTML = '';
 	// Add array elements to the container
